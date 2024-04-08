bddb485fe2e592d7e719697ee2277552ca27ca49
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2024-04-07 21:17:42 -0400

    Support multiple assistants for tasks

--- src/control_flow/settings.py
+++ src/control_flow/settings.py
@@ -1,5 +1,6 @@
 import os
 
+from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -17,10 +18,29 @@ class ControlFlowSettings(BaseSettings):
     )
 
 
+class PrefectSettings(ControlFlowSettings):
+    """
+    All settings here are used as defaults for Prefect, unless overridden by env vars.
+    """
+
+    PREFECT_LOGGING_LEVEL: str = "WARNING"
+
+    def apply(self):
+        import os
+
+        for k, v in self.model_dump().items():
+            if k not in os.environ:
+                os.environ[k] = v
+
+
 class Settings(ControlFlowSettings):
     assistant_model: str = "gpt-4-1106-preview"
     max_agent_iterations: int = 10
-    use_prefect: bool = True
+    prefect: PrefectSettings = Field(default_factory=PrefectSettings)
+
+    def __init__(self, **data):
+        super().__init__(**data)
+        self.prefect.apply()
 
 
 settings = Settings()
