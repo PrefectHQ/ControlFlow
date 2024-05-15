c840b4c884b7a2adf8a346ac6006776c19bb6825
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2024-05-15 18:13:28 -0400

    Move decorators

--- src/controlflow/__init__.py
+++ src/controlflow/__init__.py
@@ -1,10 +1,11 @@
 from .settings import settings
 
-from .core.flow import Flow, reset_global_flow as _reset_global_flow, flow
-from .core.task import Task, task
+from .core.flow import Flow, reset_global_flow as _reset_global_flow
+from .core.task import Task
 from .core.agent import Agent
 from .core.controller.controller import Controller
 from .instructions import instructions
+from .decorators import flow, task
 
 Flow.model_rebuild()
 Task.model_rebuild()
