48a5a9f06dca9f95d13cbb8b6bef84860a6b4a7d
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2024-06-03 10:57:00 -0400

    Move run responsibility to the agent

--- src/controlflow/utilities/asyncio.py
+++ src/controlflow/utilities/asyncio.py
@@ -1,8 +1,5 @@
 import asyncio
-import functools
-from typing import Any, Callable, Coroutine, TypeVar, cast
-
-from prefect.utilities.asyncutils import run_coro_as_sync
+from typing import TypeVar
 
 T = TypeVar("T")
 
@@ -29,66 +26,3 @@ def create_task(coro):
     BACKGROUND_TASKS.add(task)
     task.add_done_callback(BACKGROUND_TASKS.discard)
     return task
-
-
-class ExposeSyncMethodsMixin:
-    """
-    A mixin that can take functions decorated with `expose_sync_method`
-    and automatically create synchronous versions.
-    """
-
-    def __init_subclass__(cls, **kwargs: Any) -> None:
-        super().__init_subclass__(**kwargs)
-        for method in list(cls.__dict__.values()):
-            if callable(method) and hasattr(method, "_sync_name"):
-                sync_method_name = method._sync_name
-                setattr(cls, sync_method_name, method._sync_wrapper)
-
-
-def expose_sync_method(name: str) -> Callable[..., Any]:
-    """
-    Decorator that automatically exposes synchronous versions of async methods.
-    Note it doesn't work with classmethods.
-
-    Args:
-        name: The name of the synchronous method.
-
-    Returns:
-        The decorated function.
-
-    Example:
-        Basic usage:
-        ```python
-        class MyClass(ExposeSyncMethodsMixin):
-
-            @expose_sync_method("my_method")
-            async def my_method_async(self):
-                return 42
-
-        my_instance = MyClass()
-        await my_instance.my_method_async() # returns 42
-        my_instance.my_method()  # returns 42
-        ```
-    """
-
-    def decorator(
-        async_method: Callable[..., Coroutine[Any, Any, T]],
-    ) -> Callable[..., Coroutine[Any, Any, T]]:
-        @functools.wraps(async_method)
-        def sync_wrapper(*args: Any, **kwargs: Any) -> T:
-            coro = async_method(*args, **kwargs)
-            return run_coro_as_sync(coro)
-
-        # Cast the sync_wrapper to the same type as the async_method to give the
-        # type checker the needed information.
-        casted_sync_wrapper = cast(Callable[..., T], sync_wrapper)
-
-        # Attach attributes to the async wrapper
-        setattr(async_method, "_sync_wrapper", casted_sync_wrapper)
-        setattr(async_method, "_sync_name", name)
-
-        # return the original async method; the sync wrapper will be added to
-        # the class by the init hook
-        return async_method
-
-    return decorator
