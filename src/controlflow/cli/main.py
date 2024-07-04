32e74ed20e6247bc537534a3c6db3da0b5f6930b
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2024-07-03 20:08:44 -0400

    Add diagnostic CLI

---
+++ src/controlflow/cli/main.py
@@ -0,0 +1,48 @@
+import platform
+from pathlib import Path
+
+import langchain_core
+import prefect
+import typer
+from controlflow import __version__
+from controlflow.utilities.rich import console
+from rich.table import Table
+from typer import Context, Exit
+
+app = typer.Typer(no_args_is_help=True)
+
+
+@app.command()
+def version(ctx: Context):
+    if ctx.resilient_parsing:
+        return
+
+    info = {
+        "ControlFlow version": __version__,
+        "Prefect version": prefect.__version__,
+        "LangChain Core version": langchain_core.__version__,
+        "Python version": platform.python_version(),
+        "Platform": platform.platform(),
+        "Path": Path(__file__).resolve().parents[3],
+    }
+
+    g = Table.grid(padding=(0, 1))
+    g.add_column(justify="right")
+    g.add_column()
+    for k, v in info.items():
+        g.add_row(k + ":", str(v).replace("\n", " "))
+    console.print(g)
+
+    raise Exit()
+
+
+# this callback only exists to force `version` to be called as `controlflow
+# version` instead of as the default command, which is the default behavior when
+# there's only one command. It can be deleted if/when more commands are added.
+@app.callback()
+def callback():
+    pass
+
+
+if __name__ == "__main__":
+    app()
